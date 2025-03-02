6c75dc99233193894ded2744cbaed7869b90ce74
quocanhh86 <160404811+quocanhh86@users.noreply.github.com>
2025-03-03 01:21:57 +0700

    closed selenium

--- src/test/java/TestBai3.java
+++ src/test/java/TestBai3.java
@@ -13,32 +13,32 @@ import java.time.Duration;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestBai3 {
-    ChromeDriver chromeDriver;
-        @BeforeEach
-        public void setUp(){
-            WebDriverManager.chromedriver().setup();
-            chromeDriver = new ChromeDriver();
-        }
-
-        @Test
-        public void test() throws InterruptedException {
-            chromeDriver.get("https://lamia.com.vn/danh-muc/dam");
-            WebElement btnImage = chromeDriver.findElement(By.xpath("(//i[@class='feather icon icon-shopping-cart'])[3]"));
-            btnImage.click();
-
-            WebDriverWait wait = new WebDriverWait(chromeDriver, Duration.ofSeconds(10));
-//            Thread.sleep(2000);
-            WebElement inputPhone = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("cus_phone")));
-            inputPhone.sendKeys("0123456389");
-
-            WebElement btnAdd = chromeDriver.findElement(By.xpath("(//button[@type='submit'][contains(text(),'Gửi')])[2]"));
-            btnAdd.click();
-
-//            WebElement webElement = chromeDriver.findElement(By.xpath("//*[@id=\"ShoeType\"]"));
+//    ChromeDriver chromeDriver;
+//        @BeforeEach
+//        public void setUp(){
+//            WebDriverManager.chromedriver().setup();
+//            chromeDriver = new ChromeDriver();
+//        }
 //
-//            String actualFirstCategory = webElement.getText(); so sánh
-//            String expectedFirstCategory = "Formal Shoes";
-//            assertEquals(expectedFirstCategory, actualFirstCategory);
-
-        }
+//        @Test
+//        public void test() throws InterruptedException {
+//            chromeDriver.get("https://lamia.com.vn/danh-muc/dam");
+//            WebElement btnImage = chromeDriver.findElement(By.xpath("(//i[@class='feather icon icon-shopping-cart'])[3]"));
+//            btnImage.click();
+//
+//            WebDriverWait wait = new WebDriverWait(chromeDriver, Duration.ofSeconds(10));
+////            Thread.sleep(2000);
+//            WebElement inputPhone = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("cus_phone")));
+//            inputPhone.sendKeys("0123456389");
+//
+//            WebElement btnAdd = chromeDriver.findElement(By.xpath("(//button[@type='submit'][contains(text(),'Gửi')])[2]"));
+//            btnAdd.click();
+//
+////            WebElement webElement = chromeDriver.findElement(By.xpath("//*[@id=\"ShoeType\"]"));
+////
+////            String actualFirstCategory = webElement.getText(); so sánh
+////            String expectedFirstCategory = "Formal Shoes";
+////            assertEquals(expectedFirstCategory, actualFirstCategory);
+//
+//        }
 }
